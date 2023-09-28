40b6c8a498e192d73da1fc27a3f04362ca9d4344
BorisKGB <flot3.sky@yandex.ru>
2023-09-28 22:32:56 +0300

    add simplified unit tests as task2 solution

--- src/test/java/ru/study/seminar2/VehicleTest.java
+++ src/test/java/ru/study/seminar2/VehicleTest.java
@@ -6,4 +6,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
 
+    @Test
+    void CarInstance() {
+        Car car = new Car("Company", "model", 1500);
+        assertInstanceOf(Vehicle.class, car);
+    }
+
+    @Test
+    void CarWheelCount() {
+        Car car = new Car("Company", "model", 1500);
+        assertEquals(car.getNumWheels(), 4);
+    }
+
+    @Test
+    void MotorcycleWheelCount() {
+        Motorcycle motorcycle = new Motorcycle("Company", "model2", 1501);
+        assertEquals(motorcycle.getNumWheels(), 2);
+    }
+
+    @Test
+    void CarSpeed() {
+        Car car = new Car("Company", "model", 1500);
+        car.testDrive();
+        assertEquals(car.getSpeed(), 60);
+    }
+
+    @Test
+    void MotorcycleSpeed() {
+        Motorcycle motorcycle = new Motorcycle("Company", "model2", 1501);
+        motorcycle.testDrive();
+        assertEquals(motorcycle.getSpeed(), 75);
+    }
+
+    @Test
+    void CarParkedSpeed() {
+        Car car = new Car("Company", "model", 1500);
+        car.testDrive();
+        car.park();
+        assertEquals(car.getSpeed(), 0);
+    }
+
+    @Test
+    void MotorcycleParkedSpeed() {
+        Motorcycle motorcycle = new Motorcycle("Company", "model2", 1501);
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertEquals(motorcycle.getSpeed(), 0);
+    }
 }
